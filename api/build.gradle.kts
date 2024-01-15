d61e85f5b2574d120851b8e4f804eed0dd238aca
Hennessy-on-melody <114402124+Hennessy-on-melody@users.noreply.github.com>
2024-01-15 14:30:42 +0900

    conf#api
    add api module configuration

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,4 +5,5 @@ version = "0.0.1"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation(project(":domain"))
 }
---
+++ api/src/main/kotlin/com/zero/api/ApiApplication.kt
@@ -0,0 +1,4 @@
+package com.zero.api
+
+class ApiApplication {
+}
